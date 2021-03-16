b5f9a87fb62fb1555f16c61680a3e531b7a2d9a7
yxxhero <aiopsclub@163.com>
2021-03-16 19:38:29 +0800

    Implement the Getter interface for the git:// protocol.
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/downloader/manager.go
+++ pkg/downloader/manager.go
@@ -307,20 +307,6 @@ func (m *Manager) downloadAll(deps []*chart.Dependency) error {
 			dep.Version = ver
 			continue
 		}
-		if strings.HasPrefix(dep.Repository, "git:") {
-			destPath := filepath.Join(m.ChartPath, "charts")
-			gitURL := strings.TrimPrefix(dep.Repository, "git:")
-			if m.Debug {
-				fmt.Fprintf(m.Out, "Downloading %s from git repo %s\n", dep.Name, gitURL)
-			}
-			dl := GitDownloader{}
-			if err := dl.DownloadTo(gitURL, dep.Version, destPath); err != nil {
-				saveError = fmt.Errorf("could not download %s: %s", gitURL, err)
-				break
-			}
-			continue
-		}
-		fmt.Fprintf(m.Out, "Downloading %s from repo %s\n", dep.Name, dep.Repository)
 
 		// Any failure to resolve/download a chart should fail:
 		// https://github.com/helm/helm/issues/1439
@@ -353,6 +339,7 @@ func (m *Manager) downloadAll(deps []*chart.Dependency) error {
 		}
 
 		version := ""
+
 		if strings.HasPrefix(churl, "oci://") {
 			if !resolver.FeatureGateOCI.IsEnabled() {
 				return errors.Wrapf(resolver.FeatureGateOCI.Error(),
@@ -366,6 +353,17 @@ func (m *Manager) downloadAll(deps []*chart.Dependency) error {
 			dl.Options = append(dl.Options,
 				getter.WithRegistryClient(m.RegistryClient),
 				getter.WithTagName(version))
+		}
+
+		if strings.HasPrefix(churl, "git:") {
+			version = dep.Version
+
+			dl.Options = append(dl.Options, getter.WithTagName(version))
+			dl.Options = append(dl.Options, getter.WithChartName(dep.Name))
+
+			if m.Debug {
+				fmt.Fprintf(m.Out, "Downloading %s from git repo %s\n", dep.Name, churl)
+			}
 		}
 
 		_, _, err = dl.DownloadTo(churl, version, destPath)
@@ -714,6 +712,10 @@ func (m *Manager) parallelRepoUpdate(repos []*repo.Entry) error {
 //
 // If it finds a URL that is "relative", it will prepend the repoURL.
 func (m *Manager) findChartURL(name, version, repoURL string, repos map[string]*repo.ChartRepository) (url, username, password string, insecureskiptlsverify, passcredentialsall bool, caFile, certFile, keyFile string, err error) {
+	if strings.HasPrefix(repoURL, "git:") {
+		return repoURL, "", "", nil
+	}
+
 	if strings.HasPrefix(repoURL, "oci://") {
 		return fmt.Sprintf("%s/%s:%s", repoURL, name, version), "", "", false, false, "", "", "", nil
 	}
